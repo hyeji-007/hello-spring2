69e8c46766b5f460ef472983fe750175c312bd35
Hyeji Kwon <kwonhyeji007@gmail.com>
2025-05-22 16:48:34 +0900

    mvc와 템플릿 엔진 study

--- src/main/java/hello/hello_spring/controller/HelloController.java
+++ src/main/java/hello/hello_spring/controller/HelloController.java
@@ -3,13 +3,27 @@ package hello.hello_spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 
     @GetMapping("hello")
     public String hello(Model model) {
-        model.addAttribute("data", "spring!!");
+        model.addAttribute("data", "hello!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam("name") String name, Model model ) {
+        model.addAttribute("name", name);
+        return "hello-template";
+    }
+
+    @GetMapping("hello-spring")
+    @ResponseBody //http의 body부에 이 데이터를 직접 넣어주겠다.
+    public String helloSpring(@RequestParam("name") String name) {
+        return "hello " + name;
     }
 }
