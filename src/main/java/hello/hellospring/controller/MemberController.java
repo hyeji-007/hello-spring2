96fc60fd36f5528959080fc4a0fbb4eeb297ebbd
Hyeji Kwon <kwonhyeji007@gmail.com>
2025-05-31 18:52:43 +0900

    회원 웹 기능-등록, 조회

--- src/main/java/hello/hellospring/controller/MemberController.java
+++ src/main/java/hello/hellospring/controller/MemberController.java
@@ -1,8 +1,14 @@
 package hello.hellospring.controller;
 
+import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.List;
 
 @Controller
 public class MemberController {
@@ -12,5 +18,28 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+    @GetMapping("/members/new")
+    public String createFrom() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        System.out.println("member: " + member.getName());
+
+        memberService.join(member);
+
+        return "redirect:/"; //회원 가입 후 홈화면으로 돌려보냄
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+        model.addAttribute("members", members); //addAttribute: 컨트롤러에서 뷰로 데이터 전달
+        return "members/memberList";
     }
 }
